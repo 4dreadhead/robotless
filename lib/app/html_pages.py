316308213c228c433531f4d08626dba89e60eb59
4dreadhead <slava14042000@mail.ru>
2023-12-04 18:20:08 +0700

    Debug

--- lib/app/html_pages.py
+++ lib/app/html_pages.py
@@ -1,12 +1,22 @@
+import os
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class HtmlPages:
     def __init__(self, app):
         self.app = app
         self.templates = Jinja2Templates(directory="templates")
+
+        if os.getenv("API_HOST") in ["localhost", "127.0.0.1", "0.0.0.0"]:
+            self.base = ""
+        elif os.getenv("API_SUBDOMAIN_ALIAS"):
+            self.base = "https://" + os.getenv("API_SUBDOMAIN_ALIAS") + os.getenv("API_HOST")
+        else:
+            self.base = "https://" + os.getenv("API_HOST")
 
         self.register_handlers()
 
@@ -15,7 +25,7 @@ class HtmlPages:
         self.app.get("/share", response_class=HTMLResponse)(self.read_share)
 
     async def read_analyze(self, request: Request):
-        return self.templates.TemplateResponse("analyze.html", {"request": request})
+        return self.templates.TemplateResponse("analyze.html", {"request": request, "base_url": self.base})
 
     async def read_share(self, request: Request):
-        return self.templates.TemplateResponse("share.html", {"request": request})
+        return self.templates.TemplateResponse("share.html", {"request": request, "base_url": self.base})
