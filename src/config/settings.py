fced71020bd28ee958e5be0dda89710bdffdd913
4dreadhead <slava14042000@mail.ru>
2024-02-06 04:06:32 +0700

    fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_MODE")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("API_HOST")]
 
 
 # Application definition
