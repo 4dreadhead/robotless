82f7e198c2fa3cd369c5661a6d2066bc95a4815b
4dreadhead <slava14042000@mail.ru>
2024-02-15 18:08:41 +0700

    Finalize

--- src/config/settings.py
+++ src/config/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "src" / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
