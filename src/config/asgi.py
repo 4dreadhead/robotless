31812320ed8b772a5da32489556a265b894b9c23
4dreadhead <slava14042000@mail.ru>
2024-02-15 18:49:48 +0700

    Finalize

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings')
 
 application = get_asgi_application()
+application = WhiteNoise(application, root='src/staticfiles')
